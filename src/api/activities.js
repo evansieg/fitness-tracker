29a92cdc95a007673d4aa2caf47619dd12809ba7
Evan Siegel <evan@casasiegel.com>
2026-05-21 11:25:28 -0600

    completed, followd along with Byron and utilized solutions page

--- src/api/activities.js
+++ src/api/activities.js
@@ -12,6 +12,18 @@ export async function getActivities() {
   }
 }
 
+/** Fetches an activity by ID from the API. */
+export async function getActivity(id) {
+  try {
+    const response = await fetch(API + "/activities/" + id);
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
 /**
  * Sends a new activity to the API to be created.
  * A valid token is required.
@@ -28,6 +40,26 @@ export async function createActivity(token, activity) {
       Authorization: "Bearer " + token,
     },
     body: JSON.stringify(activity),
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
+
+/**
+ * Requests the API to delete the activity with the given ID.
+ * A valid token is required.
+ */
+export async function deleteActivity(token, id) {
+  if (!token) {
+    throw Error("You must be signed in to delete an activity.");
+  }
+
+  const response = await fetch(API + "/activities/" + id, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
   });
 
   if (!response.ok) {
